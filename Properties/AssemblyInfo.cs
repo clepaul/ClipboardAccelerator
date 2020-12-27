40cb6ad06f1dc687140844f8d2684522b740bf3b
Clemens Paul <clemenspaul@gmx.net>
2020-12-27 15:25:38 +0100

    Version number update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("0.0.23.*")]
+[assembly: AssemblyVersion("0.0.30.*")]
